02db74dc5c807a80fb15774b2e40ec773f3ccaef
Jiamin <jianminmu@microsoft.com>
2022-04-08 10:48:34 +0800

    add list indicators func

--- cloudlib/azurecloud/sentinel.go
+++ cloudlib/azurecloud/sentinel.go
@@ -4,36 +4,25 @@ import (
 	"context"
 	"log"
 
-	"go.uber.org/zap"
-
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/securityinsights/armsecurityinsights"
 )
 
-func CheckIndicatorStatus(client *armsecurityinsights.ThreatIntelligenceIndicatorClient, ctx context.Context, resourcegroupName, indicatorName, workspaceName, paternType, patern, source string) bool {
-	pager := client.QueryIndicators(resourcegroupName, workspaceName, armsecurityinsights.ThreatIntelligenceFilteringCriteria{
-		IncludeDisabled: to.BoolPtr(false),
-		PatternTypes:    []*string{to.StringPtr(paternType)},
-		Keywords:        []*string{to.StringPtr(patern)},
-		Sources:         []*string{to.StringPtr(source)},
-	}, &armsecurityinsights.ThreatIntelligenceIndicatorClientQueryIndicatorsOptions{})
+func ListIndicators(client *armsecurityinsights.ThreatIntelligenceIndicatorsClient, ctx context.Context, resourcegroupName, workspaceName string) {
+	pager := client.List(resourcegroupName, workspaceName, &armsecurityinsights.ThreatIntelligenceIndicatorsClientListOptions{})
 	for {
 		nextResult := pager.NextPage(ctx)
 		if err := pager.Err(); err != nil {
-			zap.S().Error(err.Error())
+			log.Printf("failed to get next page: %s", err.Error())
+			break
 		}
 		if !nextResult {
 			break
 		}
-		if len(pager.PageResponse().Value) == 0 {
-			return false
-		}
-		for _, item := range pager.PageResponse().Value {
-			indicator := item.GetThreatIntelligenceInformation()
-			zap.S().Infof("%s has been found", *indicator.Name)
+		for _, v := range pager.PageResponse().Value {
+			result := v.GetThreatIntelligenceInformation()
+			log.Println(*result.ID, *result.Name, *result.Kind, *result.Type)
 		}
 	}
-	return true
 }
 
 func CheckIndicator(client *armsecurityinsights.ThreatIntelligenceIndicatorClient, ctx context.Context, resourcegroupName, workspaceName, indicatorName string) bool {
